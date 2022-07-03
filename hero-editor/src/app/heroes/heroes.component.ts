d345898a35d3a3742ceb4beae1c101eb30b658c9
Grigorii Iavorskii <yavgan06@gmail.com>
2022-07-03 10:26:18 +0400

    feat: display selected hero

--- hero-editor/src/app/heroes/heroes.component.ts
+++ hero-editor/src/app/heroes/heroes.component.ts
@@ -12,11 +12,18 @@ export class HeroesComponent implements OnInit {
     id: 1,
     name: 'Windstorm'
   };
+
+  selectedHero?: IHero;
+
   heroes = HEROES;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onSelect(hero: IHero): void {
+    this.selectedHero = hero;
   }
 
 }
