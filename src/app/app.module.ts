260bcf7c8daa3d3026acae6e5c1f9cf8799849bc
Grigorii Iavorskii <yavgan06@gmail.com>
2022-07-06 10:51:15 +0400

    refactor: change module for articles declaration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,26 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './core/header/header.component';
 import { PaginationComponent } from './core/pagination/pagination.component';
-import { ArticleImgTopLeftComponent } from './articles/article-img-top-left/article-img-top-left.component';
-import { ArticleImgTopLeftSingleHeaderComponent } from './articles/article-img-top-left-single-header/article-img-top-left-single-header.component';
-import { ArticleImgTopMiddleComponent } from './articles/article-img-top-middle/article-img-top-middle.component';
-import { ArticleImgBottomRightComponent } from './articles/article-img-bottom-right/article-img-bottom-right.component';
-import { ArticleImgBottomRightSingleHeaderComponent } from './articles/article-img-bottom-right-single-header/article-img-bottom-right-single-header.component';
+import { BookModule } from './book/book.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     PaginationComponent,
-    ArticleImgTopLeftComponent,
-    ArticleImgTopLeftSingleHeaderComponent,
-    ArticleImgTopMiddleComponent,
-    ArticleImgBottomRightComponent,
-    ArticleImgBottomRightSingleHeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BookModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/book/book.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ArticleImgTopLeftComponent } from '../articles/article-img-top-left/article-img-top-left.component';
+import { ArticleImgTopLeftSingleHeaderComponent } from '../articles/article-img-top-left-single-header/article-img-top-left-single-header.component';
+import { ArticleImgTopMiddleComponent } from '../articles/article-img-top-middle/article-img-top-middle.component';
+import { ArticleImgBottomRightComponent } from '../articles/article-img-bottom-right/article-img-bottom-right.component';
+import { ArticleImgBottomRightSingleHeaderComponent } from '../articles/article-img-bottom-right-single-header/article-img-bottom-right-single-header.component';
+
+
+
+@NgModule({
+  declarations: [
+    ArticleImgTopLeftComponent,
+    ArticleImgTopLeftSingleHeaderComponent,
+    ArticleImgTopMiddleComponent,
+    ArticleImgBottomRightComponent,
+    ArticleImgBottomRightSingleHeaderComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    ArticleImgTopLeftComponent,
+    ArticleImgTopLeftSingleHeaderComponent,
+    ArticleImgTopMiddleComponent,
+    ArticleImgBottomRightComponent,
+    ArticleImgBottomRightSingleHeaderComponent
+  ]
+})
+export class BookModule { }
