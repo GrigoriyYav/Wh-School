7f4cc4b52e8bac03623ec08f66ffc7315abcb714
Grigorii Iavorskii <yavgan06@gmail.com>
2022-07-07 19:02:00 +0400

    feat: add pagination functional

--- src/app/core/pagination/pagination.component.ts
+++ src/app/core/pagination/pagination.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-pagination',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PaginationComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
 
+  redirectTo(route: string): void{
+    this.router.navigate([route]);
+  }
 }
