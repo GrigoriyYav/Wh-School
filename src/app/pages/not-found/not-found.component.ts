c827e8f31770a6cb24b9290a310c34196f1bb5c3
Grigorii Iavorskii <yavgan06@gmail.com>
2022-07-08 17:32:28 +0400

    refactor: hide pagination on page notfound

--- src/app/pages/not-found/not-found.component.ts
+++ src/app/pages/not-found/not-found.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-not-found',
   templateUrl: './not-found.component.html',
   styleUrls: ['./not-found.component.scss']
 })
-export class NotFoundComponent implements OnInit {
-
+export class NotFoundComponent implements OnInit, OnDestroy {
+  pagination?: HTMLElement;
   constructor() { }
 
   ngOnInit(): void {
+    if(document.getElementsByClassName('paginationBar').length > 0){
+      Array.from(document.getElementsByClassName('paginationBar') as HTMLCollectionOf<HTMLElement>)[0].style.display = 'none';
+    }    
   }
 
+  ngOnDestroy(): void {
+    if(document.getElementsByClassName('paginationBar').length > 0){
+      Array.from(document.getElementsByClassName('paginationBar') as HTMLCollectionOf<HTMLElement>)[0].style.display = 'flex';
+    }
+  }
 }
