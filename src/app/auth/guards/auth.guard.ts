d85ef12d81d71fcc7f1b565167246ef19c570f43
Grigorii Iavorskii <yavgan06@gmail.com>
2022-07-19 19:04:02 +0400

    fix: typos in snackbar

--- src/app/auth/guards/auth.guard.ts
+++ src/app/auth/guards/auth.guard.ts
@@ -19,10 +19,10 @@ export class AuthGuard implements CanActivate {
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
 
-    const loginStatys = this.auth.isLoggedIn;
+    const loginStatus = this.auth.isLoggedIn;
     this.accessToken = route.data['accessToken'];
     
-    if (loginStatys) {
+    if (loginStatus) {
       const userTokens = this.auth.userTokens;
       if(userTokens.includes(this.accessToken)){
         return true;
@@ -32,8 +32,8 @@ export class AuthGuard implements CanActivate {
         return false;
       }       
     } else {
-      this.openSnackBar(`Plese login to get accses to the page ${state.url.slice(1)}`,'OK')
-      //window.alert(`Plese login to get accses to the page ${state.url.slice(1)}`);
+      this.openSnackBar(`Please login to get access to the page ${state.url.slice(1)}`,'OK')
+      //window.alert(`Please login to get access to the page ${state.url.slice(1)}`);
       return false;
     }
   }
